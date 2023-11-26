c347ec10aeb7e979f4a2772ad0633a359a959b5d
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-26 14:36:46 -0700

    completed array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+const users = ["Monica", "Bryan", "Bob"]
+
+function findMatching(array, string) {
+    
+    return array.filter(function(element) {
+        return element.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    const stringLength = string.length;
+        return array.filter(function(element) {
+            for (let i = 0; i < stringLength; i++) {
+               return (element.charAt(i) === string.charAt(i)) 
+            }
+        })
+    }
+
+function matchName(array, string) {
+    return array.filter(function(element) {   
+        return element.name === string;
+    })
+}
